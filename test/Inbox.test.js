fd59909967ef7175732aec73ce820eac0aef5162
Andrew <andrewdavidfisher@gmail.com>
2018-05-13 18:50:30 -0700

    Setting up testing

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,38 +3,21 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+beforeEach(() => {
+  // Get a list of all accounts
+  web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    })
+  // Use one of those accoutns to deploy
+  // the contract
+});
 
+describe('Inbox', () => {
+  it('deploys a contract', () => {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
+});
 // class Car {
 //   park(){
 //     return 'stopped';
